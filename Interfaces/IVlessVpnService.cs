c1454c182ca81236afd9a1ee3d8ea1f7c10f8a93
MaxGog <max.gog2005@outlook.com>
2025-06-21 14:01:01 +0300

    Добавление разрешений на VPN для платформ

--- Interfaces/IVlessVpnService.cs
+++ Interfaces/IVlessVpnService.cs
@@ -3,9 +3,9 @@ using vtrace.Models;
 namespace vtrace.Interfaces;
 
 public interface IVlessVpnService
-    {
-        Task<bool> Connect(VlessConfig config);
-        void Disconnect();
-        bool IsConnected { get; }
-        event EventHandler<string> ConnectionStatusChanged;
-    }
+{
+    Task<bool> Connect(VlessConfig config);
+    void Disconnect();
+    bool IsConnected { get; }
+    event EventHandler<string> ConnectionStatusChanged;
+}
